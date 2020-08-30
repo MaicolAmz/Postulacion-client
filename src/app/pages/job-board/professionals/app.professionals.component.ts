bd9673bbc6c7723af2790b04a0b03810a56ecc25
OsorioCarlos <carlosalexander.2001@hotmail.com>
2020-08-30 14:46:11 -0500

    Mejora en la interfaz

--- src/app/pages/job-board/professionals/app.professionals.component.ts
+++ src/app/pages/job-board/professionals/app.professionals.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MenuItem} from 'primeng/api';
+import { MenuItem } from 'primeng/api';
 import { JobBoardService } from '../../../services/job-board/job-board.service';
 import { Professional } from '../../../models/job-board/models.index';
 
@@ -15,87 +15,38 @@ export class AppProfessionalsComponent implements OnInit {
   totalProffesionals: number;
   totalOffers: number;
 
-  constructor(private jobBoardService: JobBoardService) {}
+  constructor(private jobBoardService: JobBoardService) {
+    this.items = [
+      {
+        label: 'EDUCACION',
+        icon: 'pi pi-pw pi-file',
+        items: [
+          { label: 'ASISTENTE PEDAGOGICO CON NIVEL EQUIVALENTE A TECNOLOGO SUPERIOR', icon: 'pi pi-fw pi-plus' },
+          { label: 'ASISTENTE EN EDUCACION INCLUSIVA CON NIVEL EQUIVALENTE A TECNOLOGO SUPERIOR', icon: 'pi pi-fw pi-external-link' }
+        ]
+      },
+      {
+        label: 'CIENCIAS SOCIALES, PERIODISMO E INFORMACION',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          { label: 'PRODUCTOR Y CONDUCTOR DE RADIO CON NIVEL EQUIVALENTE A TECNOLOGO SUPERIOR', icon: 'pi pi-fw pi-trash' },
+          { label: 'PRODUCTOR Y CONDUCTOR DE TELEVISION CON NIVEL EQUIVALENTE A TECNOLOGO SUPERIOR', icon: 'pi pi-fw pi-refresh' }
+        ]
+      },
+      {
+        label: 'ADMINISTRACION',
+        icon: 'pi pi-fw pi-question',
+        items: [
+          { label: 'TECNOLOGO SUPERIOR EN TRIBUTACION', icon: 'pi pi-pi pi-bars' },
+          { label: 'TECNOLOGO SUPERIOR EN AUDITORIA', icon: 'pi pi-pi pi-search' }
+        ]
+      }
+    ];
+  }
 
   ngOnInit() {
     this.getTotal();
     this.getProfessionals();
-
-    this.items = [
-      {
-        label: 'File',
-        icon: 'pi pi-pw pi-file',
-        items: [{
-          label: 'New',
-          icon: 'pi pi-fw pi-plus',
-          items: [
-            { label: 'User', icon: 'pi pi-fw pi-user-plus' },
-            { label: 'Filter', icon: 'pi pi-fw pi-filter' }
-          ]
-        },
-        { label: 'Open', icon: 'pi pi-fw pi-external-link' },
-        { separator: true },
-        { label: 'Quit', icon: 'pi pi-fw pi-times' }
-        ]
-      },
-      {
-        label: 'Edit',
-        icon: 'pi pi-fw pi-pencil',
-        items: [
-          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
-          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
-        ]
-      },
-      {
-        label: 'Help',
-        icon: 'pi pi-fw pi-question',
-        items: [
-          {
-            label: 'Contents',
-            icon: 'pi pi-pi pi-bars'
-          },
-          {
-            label: 'Search',
-            icon: 'pi pi-pi pi-search',
-            items: [
-              {
-                label: 'Text',
-                items: [
-                  {
-                    label: 'Workspace'
-                  }
-                ]
-              },
-              {
-                label: 'User',
-                icon: 'pi pi-fw pi-file',
-              }
-            ]
-          }
-        ]
-      },
-      {
-        label: 'Actions',
-        icon: 'pi pi-fw pi-cog',
-        items: [
-          {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-pencil',
-            items: [
-              { label: 'Save', icon: 'pi pi-fw pi-save' },
-              { label: 'Update', icon: 'pi pi-fw pi-save' },
-            ]
-          },
-          {
-            label: 'Other',
-            icon: 'pi pi-fw pi-tags',
-            items: [
-              { label: 'Delete', icon: 'pi pi-fw pi-minus' }
-            ]
-          }
-        ]
-      }
-    ];
   }
 
   getProfessionals(): void {
